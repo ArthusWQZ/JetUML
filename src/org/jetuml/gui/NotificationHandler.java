f5ac5d46194a632a2b1afee02ddcb2f6b37f1314
ArthusWQZ <arthus.wauquiez@neuf.fr>
2023-09-14 18:30:21 -0400

    Created NotificationHandler class following the Singleton design pattern

---
+++ src/org/jetuml/gui/NotificationHandler.java
@@ -0,0 +1,25 @@
+package org.jetuml.gui;
+
+import javafx.stage.Stage;
+
+import java.util.LinkedList;
+
+public class NotificationHandler {
+
+    private static final NotificationHandler INSTANCE = new NotificationHandler();
+
+    private Stage aMainStage;
+    private LinkedList<ToastNotification> aNotificationList = new LinkedList<>();
+
+    private NotificationHandler() {}
+
+    public static NotificationHandler getInstance() { return INSTANCE; }
+
+    public void setStage(Stage pMainStage) {
+        this.aMainStage = pMainStage;
+    }
+
+
+    public void spawn(String pText) {
+    }
+}
