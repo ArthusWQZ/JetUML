314b7ad223ff7d8c3d9d65c7cf7dfa829d262d6d
ArthusWQZ <arthus.wauquiez@neuf.fr>
2023-09-14 20:28:25 -0400

    Implemented vertical stacking when multiple notifications

--- src/org/jetuml/gui/NotificationHandler.java
+++ src/org/jetuml/gui/NotificationHandler.java
@@ -19,6 +19,16 @@ public class NotificationHandler {
         this.aMainStage = pMainStage;
     }
 
+    private void updateVerticalPosition(int pOffset) {
+
+        double y = this.aMainStage.getY() + this.aMainStage.getHeight() - 18;
+        for (int i = 0; i < aNotificationList.size(); i++) {
+            ToastNotification toast = aNotificationList.get(i);
+            toast.setY( y - 40*( (aNotificationList.size() - 1 - i) + pOffset) );
+        }
+
+    }
+
     public void spawn(String pText) {
 
         if (this.aMainStage == null) return;
@@ -27,7 +37,15 @@ public class NotificationHandler {
         double y = this.aMainStage.getY()+this.aMainStage.getHeight()-18;
 
         ToastNotification toast = new ToastNotification(pText, this.aMainStage);
+
+        this.updateVerticalPosition(1);
+        aNotificationList.add(toast);
+
+        System.out.println(aNotificationList.size());
+
         toast.show(x, y);
+
+        // TODO: remove toast from aNotificationList
 
     }
 }
